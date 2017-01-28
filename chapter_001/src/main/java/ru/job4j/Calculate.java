dca7d670e00bf3df156c3fecd0e9da560c18c6d8
MikhailKlimentiev <mikhal.klimentiev@gmail.com>
2017-01-28 20:33:34 +0300

    Add comment text

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,11 @@
 package ru.job4j;
 
+/**
+ * Class Calculate решение задачи 1.1. Создание Hello World
+ * @author mklimentiev
+ * @since 28.01.2017
+ * @version 2
+*/
 public class Calculate {
 	public static void main(String[] args) {
 		System.out.println("Hello world.");
