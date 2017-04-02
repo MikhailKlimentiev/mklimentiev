eb537254ada5fe8865eca4ad6c57710749c6908f
MikhailKlimentiev <mikhal.klimentiev@gmail.com>
2017-04-02 11:58:07 +0300

    add Counter.java

---
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -0,0 +1,28 @@
+package ru.job4j.loop;
+
+/**
+ * Counter.
+ *
+ * @author mklimentiev
+ * @version 1.0
+ * @since 01.04.2017
+*/
+public class Counter {
+
+	/**
+	 * add.
+	 * The method add(int start, int finish) calculates and returns the sum of even numbers in the range from start to finish
+	 * @param start -- the first parameter
+	 * @param finish -- the second parameter
+	 * @return -- the sum of even numbers in the range from start to finish
+	*/
+	public int add(int start, int finish) {
+		int sumOfEvenNumbers = 0;
+		for (int i = start; i <= finish; i++) {
+			if (i % 2 == 0) {
+				sumOfEvenNumbers = sumOfEvenNumbers + i;
+			}
+		}
+		return sumOfEvenNumbers;
+	}
+}
