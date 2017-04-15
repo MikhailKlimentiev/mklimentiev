43a683c9c0283aaf65ecf245a08562030f512a84
MikhailKlimentiev <mikhal.klimentiev@gmail.com>
2017-04-15 21:27:50 +0300

    add Paint.java

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -12,19 +12,22 @@ public class Paint {
 	 * piramid.
 	 * The method piramid(int h) builds a pyramid according to height that pass as a parameter;
 	 * @param h -- the first parameter;
+	 * @return the piramid like String type
 	*/
-	public void piramid(int h) {
+	public String piramid(int h) {
 		StringBuilder builder = new StringBuilder();
 		for (int i = 1; i <= h; i++) {
 			for (int j = 1; j <= h * 2 - 1; j++) {
-				if (j > h- i && j < h + i) {
-					builder.append("*");
-				} else {  
+				if (j > h - i && j < h + i) {
+					builder.append("^");
+				} else {
 					builder.append(" ");
 				}
 			}
-			System.getProperty("line.separator");
-		} 
-		System.out.printlln(builder.toString());
+			if (i < h) {
+				builder.append(System.getProperty("line.separator"));
+			}
+		}
+		return builder.toString();
 	}
 }
