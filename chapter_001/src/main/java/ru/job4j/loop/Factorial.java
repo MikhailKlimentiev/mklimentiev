034f25b0800422c6156e32c415aa63403b847d0f
MikhailKlimentiev <mikhal.klimentiev@gmail.com>
2017-04-15 21:42:05 +0300

    factorial *= i

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -18,7 +18,7 @@ public class Factorial {
 	public int calc(int n) {
 		int factorial = 1;
 		for (int i = 1; i <= n; i++) {
-			factorial = factorial * i;
+			factorial *= i;
 		}
 		return factorial;
 	}
