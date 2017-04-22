530a0eb7a424e0ad9884897819fa44fe729d4660
MikhailKlimentiev <mikhal.klimentiev@gmail.com>
2017-04-22 22:11:15 +0300

    change inner for

--- chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -16,11 +16,11 @@ public class BubbleSort {
 	*/
 	public int[] sort(int[] array) {
 		for (int i = 1; i < array.length; i++) {
-			for (int j = 1; j < array.length; j++) {
+			for (int j = array.length - 1; j >= i; j--) {
 				if (array[j] < array[j - 1]) {
-					int temp = array[j];
-					array[j] = array[j - 1];
-					array[j - 1] = temp;
+					int temp = array[j - 1];
+					array[j - 1] = array[j];
+					array[j] = temp;
 				}
 			}
 		}
