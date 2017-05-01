26f16ad080b451d9f93386890004eadf129f9afd
MikhailKlimentiev <mikhal.klimentiev@gmail.com>
2017-05-01 21:05:38 +0300

    add package-info.java

--- chapter_001/src/main/java/ru/job4j/array/package-info.java
+++ chapter_001/src/main/java/ru/job4j/array/package-info.java
@@ -3,4 +3,4 @@
  * @version 1.0
  * @since 01.04.2017
 */
-package ru.job4j.loop;
+package ru.job4j.array;
--- chapter_001/src/test/java/ru/job4j/array/package-info.java
+++ chapter_001/src/test/java/ru/job4j/array/package-info.java
@@ -3,5 +3,5 @@
  * @version 1.0
  * @since 01.04.2017
 */
-package ru.job4j.loop;
+package ru.job4j.array;
 
