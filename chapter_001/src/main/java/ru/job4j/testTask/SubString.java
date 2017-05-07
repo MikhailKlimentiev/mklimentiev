d037ff25630588eb7cd09fe1372aaabfad735aca
MikhailKlimentiev <mikhal.klimentiev@gmail.com>
2017-05-07 23:04:36 +0300

    added comments and tests

--- chapter_001/src/main/java/ru/job4j/testTask/SubString.java
+++ chapter_001/src/main/java/ru/job4j/testTask/SubString.java
@@ -4,8 +4,8 @@ package ru.job4j.testTask;
  * SubString.
  *
  * @author mklimentiev
- * @version 1.0
- * @since 30.04.2017
+ * @version 2.0
+ * @since 07.05.2017
 */
 public class SubString {
 
@@ -23,14 +23,18 @@ public class SubString {
 		char[] subChar = sub.toCharArray();
 		boolean subIsSubstringOfOrigin = false;
 
+		//There check that the length of the array subChar is greater or equal to the array originChar
 		if (subChar.length <= originChar.length) {
+			//There find the first char of the array subChar in the array originChar
+			//The search is limited (originChar.length - subChar.length + 1) because further it does not make sense
 			for (int i = 0; i < originChar.length - subChar.length + 1; i++) {
 				if (originChar[i] == subChar[0]) {
 					subIsSubstringOfOrigin = true;
-					for (int j = i + 1; j < i + originChar.length; j++) {
+					//There check that all the characters from the second to the last of the array subChar match to the array originChar
+					for (int j = i + 1; j < i + subChar.length; j++) {
 						if (originChar[j] != subChar[j - i]) {
 							subIsSubstringOfOrigin = false;
-							break;
+							return subIsSubstringOfOrigin;
 						}
 
 						if (subIsSubstringOfOrigin) {
--- chapter_001/src/test/java/ru/job4j/testTask/SubStringTest.java
+++ chapter_001/src/test/java/ru/job4j/testTask/SubStringTest.java
@@ -8,8 +8,8 @@ import static org.hamcrest.core.Is.is;
 /**
  * SubStringTest.
  * @author mklimentiev
- * @version 1.0
- * @since 30.04.2017
+ * @version 2.0
+ * @since 07.05.2017
 */
 public class SubStringTest {
 
@@ -37,5 +37,31 @@ public class SubStringTest {
 		boolean expectedValue1 = false;
 		boolean resultedValue1 = subString1.contains(origin1, sub1);
 		assertThat(resultedValue1, is(expectedValue1));
+	}
+
+	/**
+	 * Test contains.
+	*/
+	@Test
+	public void whenOriginContainsSubThenTrue2() {
+		SubString subString2 = new SubString();
+		String origin2 = "Hello World!!!";
+		String sub2 = "World!";
+		boolean expectedValue2 = true;
+		boolean resultedValue2 = subString2.contains(origin2, sub2);
+		assertThat(resultedValue2, is(expectedValue2));
+	}
+
+	/**
+	 * Test contains.
+	*/
+	@Test
+	public void whenOriginContainsSubThenTrue3() {
+		SubString subString3 = new SubString();
+		String origin3 = "777777";
+		String sub3 = "7";
+		boolean expectedValue3 = true;
+		boolean resultedValue3 = subString3.contains(origin3, sub3);
+		assertThat(resultedValue3, is(expectedValue3));
 	}
 }
