19f711e17717250d15051e9cdcefa1cc92a41458
MikhailKlimentiev <mikhal.klimentiev@gmail.com>
2017-05-07 19:54:53 +0300

    j < array.length - count

--- chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -22,7 +22,7 @@ public class ArrayDuplicate {
 		String temp = "";
 
 		for (int i = 0; i < array.length; i++) {
-			for (int j = i + 1; j < array.length; j++) {
+			for (int j = i + 1; j < array.length - count; j++) {
 				if (array[i].equals(array[j])) {
 					temp = array[j];
 					array[j] = array[array.length - 1 - count];
